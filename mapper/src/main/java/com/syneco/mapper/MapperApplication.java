722fb15d183743860061ab957cae3564529ef9d8
Viper <Viper@ViperPC>
2018-05-01 14:21:48 +0300

    Initial commit

---
+++ mapper/src/main/java/com/syneco/mapper/MapperApplication.java
@@ -0,0 +1,12 @@
+package com.syneco.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperApplication.class, args);
+	}
+}
